d7de262e639b4f5bd3e2ba232e65713da01ee9fc
sofiaguerra22 <sofiaguerrap21@gmail.com>
2022-11-17 20:05:18 -0300

    cambios

--- libreria/Funciones.cpp
+++ libreria/Funciones.cpp
@@ -23,11 +23,11 @@ bool chequearfechas(ultConsulta* Consulta)
         return true;
 
 }
-void guardararchivo(string nombrearchivo, Paciente* pac)
+void guardararchivo(fstream& archivo, Paciente* pac)
 {
-    fstream archivo;
+    
     string aux;
-    archivo.open(nombrearchivo, ios::app);
+
     if (archivo.is_open())
     {
         getline(archivo, aux);
@@ -46,18 +46,16 @@ void guardararchivo(string nombrearchivo, Paciente* pac)
 }
 
 
-paciente& buscarPaciente(string ArchivoCompleto, string _id) {
+paciente& buscarPaciente(ifstream Archivocompleto, string _id) {
     string coma = ",";
     string barra = "/";
     paciente aux;
     paciente aux1;
-    fstream miArchivo;
-    miArchivo.open(ArchivoCompleto, ios::in);
-    while (miArchivo)
+    while (Archivocompleto)
     {
 
 
-        miArchivo >> aux.dni >> coma >> aux.nombre >> coma >> aux.apellido >> coma >> aux.estado >> coma >> aux.dni >> coma >> aux.sexo >> coma >> aux.nacimiento.fecha.tm_mday >> barra >> aux.nacimiento.fecha.tm_mon >> barra >> aux.nacimiento.fecha.tm_year;
+        Archivocompleto >> aux.dni >> coma >> aux.nombre >> coma >> aux.apellido >> coma >> aux.estado >> coma >> aux.dni >> coma >> aux.sexo >> coma >> aux.nacimiento.fecha.tm_mday >> barra >> aux.nacimiento.fecha.tm_mon >> barra >> aux.nacimiento.fecha.tm_year;
         if (aux.dni == _id)
         {
             aux1 = aux;
@@ -71,36 +69,67 @@ paciente& buscarPaciente(string ArchivoCompleto, string _id) {
     return aux1;
 
 }
-void recuperarPaciente(ultimaConsulta consultaPaciente, ifstream ArchivoCompleto) {
-    paciente* Pac = &buscarPaciente("ArchivoCompleto.csv", consultaPaciente.dni_pac);
+void recuperarPaciente(ifstream ArchivoConsultas, ifstream ArchivoCompleto, fstream& Archivados, fstream& ArchivoPacientes) {
+    ultimaConsulta aux;
+    char coma = ',';
+    char barra = '/';
+    string encabezado;
+    ArchivoConsultas >> encabezado >> coma >> encabezado >> barra >> encabezado >> barra >> encabezado >> barra >> encabezado >> barra >> encabezado >> barra >> encabezado >> barra >> encabezado >> coma >> encabezado;//todo ver
+    ArchivoConsultas >> aux.dni_pac >> coma >> aux.consulta.fecha.tm_mday >> barra >> aux.consulta.fecha.tm_mon >> barra >> aux.consulta.fecha.tm_year >> barra >> aux.fecha_turno.fecha.tm_mday >> barra >> aux.fecha_turno.fecha.tm_mon >> barra >> aux.fecha_turno.fecha.tm_year >> barra >> aux.presento >> coma >> aux.matricula_med;
+    paciente* Pac = &buscarPaciente(ArchivoCompleto, aux.dni_pac);
     if (Pac->dni == "404") //si el paciente esta vacio quiere decir q no lo encontre, aca hay q ver tema punteros.
     {
         cout << "No se encontro el paciente" << endl;
     }
-    else if (chequearfechas(&consultaPaciente) == true && Pac->asistencia == false) {
+    else if (chequearfechas(&aux) == true && Pac->asistencia == false) {
         if (Pac->estado == "DIFUNTO") {
             Pac->archivado = "Archivado";
-            guardararchivo("ArchivosArchivados.csv", Pac); //lo mando al archivo q no se recuperan
+            guardararchivo(Archivados, Pac); //lo mando al archivo q no se recuperan
         }
         else if (Pac->estado == "INTERNADO") {
             cout << "El paciente se encuentra internado " << endl;
-            guardararchivo("ArchivoPac", Pac);
+            guardararchivo(ArchivoPacientes, Pac);
         }
         else {
-            informarsecretaria(*Pac); //se lo mando a secretaria para preguntarle al paciente si vuelve
-            guardararchivo("ArchivoPac.csv", Pac); // lo guardo para despues preguntar si vuelve
+            informarsecretaria(*Pac,); //se lo mando a secretaria para preguntarle al paciente si vuelve
+            guardararchivo(ArchivoPacientes, Pac); // lo guardo para despues preguntar si vuelve
         }
     }// si pasaron menos de 10 años y no reprogramo puede volver
 }
-void informarsecretaria(Paciente& pac)
+
+void informarsecretaria(Paciente& pac, fstream& Archivados, fstream Archivocontactos)
 {
+    int n = 0;
+    Contacto* contactoaux = new Contacto[n];
+    Contacto contact;
     string aux;
-    cout << "Llamando al paciente: " << pac.nombre << pac.apellido << "Su numero es: " << pac.cont.telefono;
+    string encabezado;
+    char coma = ',';
+    int i = 0;
+    while (Archivocontactos) {
+        if (i == 0)
+        {
+            Archivocontactos >> encabezado >> coma >> encabezado >> coma >> encabezado >> coma >> encabezado >> coma >> encabezado;
+            i++;
+        }
+        else
+        {
+            Archivocontactos >> contactoaux[n].dni_pac >> coma >> contactoaux[n].telefono >> coma >> contactoaux[n].celular >> coma >> contactoaux[n].direccion >> coma >> contactoaux[n].email;
+            resizecontacto(contactoaux, &n);
+        }
+    }
+    for (int k = 0;k < n;k++) {
+        if (contactoaux[k].dni_pac == pac.dni) {
+            contact = contactoaux[k];
+        }
+    }
+
+    cout << "Llamando al paciente: " << pac.nombre << pac.apellido << "Su numero es: " << contact.celular << endl;
     cout << "Vuelve";
     cin >> aux;
     if (aux == "si" || aux == "Si" || aux == "SI" || aux == "sI")
     {
-        cout << "VUelve el paciente";
+        cout << "Vuelve el paciente";
         pac.retorno = true;
         cout << "ingrese su nueva obra social";
         if (pac.obra_social != aux)
@@ -112,49 +141,55 @@ void informarsecretaria(Paciente& pac)
     {
         cout << "No vuelve el paciente";
         pac.archivado = "archivado";
-        guardararchivo("ArchivosArchivados.csv", &pac);
+        guardararchivo(Archivados, &pac);
     }
 }
-void guardararchivocontacto(string nombrearchivo, contacto cont)
+void guardararchivocontacto(fstream &Archivo, contacto Contacto)
 {
-    fstream archivo;
     string aux;
-    archivo.open(nombrearchivo, ios::app);
-    if (archivo.is_open())
+    if (Archivo.is_open())
     {
-        getline(archivo, aux);
+        getline(Archivo, aux);
 
         if (aux == "\0") //si en la primer linea no hay nada,tengo que poner el encabezad
         {
-            archivo << "Dni" << "telefono" << "celular" << "direccion" << "mail" << endl;
+            Archivo << "Dni" << "telefono" << "celular" << "direccion" << "mail" << endl;
         }
         else
         {
-            archivo << cont.dni_pac << cont.telefono << cont.celular << cont.direccion << cont.email << endl; //todo preguntar
+            Archivo << Contacto.dni_pac << Contacto.telefono << Contacto.celular << Contacto.direccion << Contacto.email << endl; //todo preguntar
         }
     }
-    archivo.close();
 
 }
-void guardarmedicos(string nombrearchivo, medico medico)
+void guardarmedicos(fstream &Archivo, medico medico)
 {
-    fstream archivo;
     string aux;
     char coma = ',';
-    archivo.open(nombrearchivo, ios::app);
-    if (archivo.is_open())
+    if (Archivo.is_open())
     {
-        getline(archivo, aux);
+        getline(Archivo, aux);
 
         if (aux == "\0") //si en la primer linea no hay nada,tengo que poner el encabezad
         {
-            archivo << "matricula" << coma << "nombre" << coma << "apellido" << coma << "telefono" << coma << "especialidad" << coma << "activo" << endl;
+            Archivo << "matricula" << coma << "nombre" << coma << "apellido" << coma << "telefono" << coma << "especialidad" << coma << "activo" << endl;
         }
         else
         {
-            archivo << medico.matricula << coma << medico.nombre << coma << medico.apellido << coma << medico.telefono << coma << medico.especialidad << coma << medico.activo << endl; //todo preguntar
+            Archivo << medico.matricula << coma << medico.nombre << coma << medico.apellido << coma << medico.telefono << coma << medico.especialidad << coma << medico.activo << endl; //todo preguntar
         }
     }
-    archivo.close();
+}
+void resizecontacto(Contacto*& vector, int* n)
+{
+    *n = *(n)+1; //aumento en uno mi contador de tamanio
+    Contacto* aux = new Contacto[*(n)]; //me hago un auxiliar con un nuevo tamanio
 
+    for (int i = 0; i < *n - 1; i++) //es hasta n-1 ya que mi vector todavia tiene tamanio n
+    {
+        aux[i] = vector[i]; //copio las cosas en el auxiliar
+    }
+    delete[]vector; //libero la memoria de donde estaba vector
+
+    vector = aux; //le asigno la nueva memoria
 }
